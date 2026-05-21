bf8e2f7f1572314e940a887d160273ce57bea1ac
Mathetes <260054114+mathetes2602@users.noreply.github.com>
2026-05-21 09:02:49 +0300

    start project

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("api key not found")
+
+client = genai.Client(api_key=api_key)
+
+def main():
+    print("Hello from ai-agent!")
+
+
+if __name__ == "__main__":
+    main()
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=args.user_prompt
+)
+if response.usage_metadata == None:
+    raise RuntimeError("failed api request")
+
+print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+print("Response tokens: ", response.usage_metadata.candidates_token_count)
+print(response.text)
