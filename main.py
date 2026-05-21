e1f0e9d5046f96035efd9c77f6501ffc8c68bb0b
Mathetes <260054114+mathetes2602@users.noreply.github.com>
2026-05-21 09:21:15 +0300

    add verbose output

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
@@ -21,6 +20,7 @@ if __name__ == "__main__":
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 messages: list[types.Content] = [
@@ -34,6 +34,10 @@ response = client.models.generate_content(
 if response.usage_metadata == None:
     raise RuntimeError("failed api request")
 
-print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-print("Response tokens: ", response.usage_metadata.candidates_token_count)
-print(response.text)
+if args.verbose:
+    print("User prompt: ", args.user_prompt)
+    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+    print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    print(response.text)
+else:
+    print(response.text)
